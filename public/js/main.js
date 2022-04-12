2449ccb863a4cfda145994152dfccc9e643ac556
Илья Матвеев <67162465+AcidRainFckU@users.noreply.github.com>
2022-04-12 16:50:11 +0300

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+new WOW().init();
+
+// SCROLL
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+// ОТКРЫТИЕ БЛОКОВ
+const openBlock = document.querySelectorAll(".open-block");
+const title = document.querySelectorAll(".title");
+
+title.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    if (!openBlock[i].classList.contains("opened")) {
+      openBlock.forEach((elem) => elem.classList.remove("opened"));
+      openBlock[i].classList.add("opened");
+    } else {
+      openBlock[i].classList.remove("opened");
+    }
+  });
+});
+
+// ЗАМЕНА СВГ
+
+const svgRenew = document.querySelectorAll(".svg-renew");
+
+function renewImg(elem, num, form = ".svg") {
+  let img = elem.getAttribute("src");
+  let srcset = elem.previousElementSibling;
+  let newimg;
+
+  img = img.replace(/[0-9]/g, "");
+
+  if (img[img.length - 5] !== num) {
+    newimg = img.split(form).join(num + form);
+  }
+
+  elem.setAttribute("src", newimg);
+  srcset.setAttribute("srcset", newimg);
+}
+
+function startRenew() {
+  let width = window.innerWidth;
+  if (width < 768) {
+    svgRenew.forEach((el) => {
+      renewImg(el, "");
+    });
+  } else if (width < 1210) {
+    svgRenew.forEach((el) => {
+      renewImg(el, "2");
+    });
+  } else if (width > 1210) {
+    svgRenew.forEach((el) => {
+      renewImg(el, "3");
+    });
+  }
+}
+
+window.addEventListener("resize", () => {
+  startRenew();
+});
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  startRenew();
+});
